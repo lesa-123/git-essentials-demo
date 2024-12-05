c8417ae03bbbe06d3bb8b80419c908f00bf037b5
PALESA MCHUNU <LESA_12@ICLOUD.COM>
2024-12-05 14:45:33 +0000

    Add code# cd

---
+++ app.js
@@ -0,0 +1,23 @@
+//I want to see a message in the console
+//In the console, perform the log action with the following message
+console.log("Test");
+
+//I want to store some data
+//we are going to use variables to store data
+//we can use two keywords to write variables: let, const
+//we only use the keyword to declare a variable --> we only use it when the box is new
+//we can only store ONE value in a variable
+const myName = "Palesa";
+let myLocation = "Leicester";
+let myJob = "Learner";
+
+//I want to see my variable in the console
+console.log(myName);
+
+//I want to store another piece of data in the same box
+//I don't declare the variable again because the variable already exists; we only declare them once.
+myName = "Joe";
+
+console.log(myName);
+
+//if we declare a variable with const, we CANNOT change the value inside; it's constant
